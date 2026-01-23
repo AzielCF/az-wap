a28ded5d4712c27fa8acd9ab84d90d090ddd0b60
Aziel <azielcruzado@gmail.com>
2026-01-23 14:33:51 -0500

    chore: bump version to v2.0.0-beta.6

--- src/config/settings.go
+++ src/config/settings.go
@@ -14,7 +14,7 @@ import (
 )
 
 var (
-	AppVersion             = "v2.0.0-beta.5"
+	AppVersion             = "v2.0.0-beta.6"
 	AppPort                = "3000"
 	AppDebug               = false
 	AppOs                  = "AzielCf"
