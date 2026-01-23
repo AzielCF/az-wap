668e81d1cade8a0d162d055bd027d8d283626f3f
Aziel <azielcruzado@gmail.com>
2026-01-23 16:04:51 -0500

    chore: debug panic with stack trace logging (v2.0.0-beta.10)

--- src/config/settings.go
+++ src/config/settings.go
@@ -14,7 +14,7 @@ import (
 )
 
 var (
-	AppVersion             = "v2.0.0-beta.9"
+	AppVersion             = "v2.0.0-beta.10"
 	AppPort                = "3000"
 	AppDebug               = false
 	AppOs                  = "AzielCf"
